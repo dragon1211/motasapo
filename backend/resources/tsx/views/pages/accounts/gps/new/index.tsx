ab9c74c93324e909f601c791bf226de029e0870a
dragon1211 <vikt0r1211@hotmail.com>
2021-07-20 11:38:54 -0700

    delete

--- backend/resources/tsx/views/pages/accounts/gps/new/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {New} from './new';
-
-if (document.getElementById('new')) {
-    ReactDOM.render(<New />, document.getElementById('new'));
-}
